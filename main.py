b80a154a5ebba389a00d758a7d9a41ca099c4df8
cogesum <ilya.neuron7@gmail.com>
2021-06-22 00:49:12 +0300

    commited

---
+++ main.py
@@ -0,0 +1,17 @@
+# pip install pytesseract pillow
+
+import pytesseract
+from PIL import Image
+
+img = Image.open('pic4.jpg')
+
+file_name = img.filename
+file_name = file_name.split(".")[0]
+print(file_name)
+
+text = pytesseract.image_to_string(img, lang='rus').strip()
+
+print(text)
+
+with open(f'{file_name}.txt', 'w') as text_file:
+    text_file.write(text)
